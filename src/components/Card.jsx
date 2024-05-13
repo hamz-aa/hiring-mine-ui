e1835c52f1bb3d80538f7395566909f19e0888b3
Hamza <jhamzaahmed@gmail.com>
2024-05-13 23:12:44 +0500

    updated card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Title = styled.h2``;
+const Text = styled.p``;
+
 const Card = () => {
-  return <Container>Card</Container>;
+  return (
+    <Container>
+      <Title>title</Title>
+      <Text>text</Text>
+    </Container>
+  );
 };
 
 export default Card;
