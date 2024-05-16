c67cfea3f67470585018f645da93333217bc5bcc
Hamza <jhamzaahmed@gmail.com>
2024-05-16 22:29:54 +0500

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Left = styled.div``;
+const Right = styled.div``;
+const Logo = styled.img``;
+const Text = styled.p``;
+const SubHeading = styled.h5``;
+const SocialMediaIcons = styled.div``;
+const BoldText = styled.p``;
+const EmailContent = styled.div``;
+const Hr = styled.hr``;
+
 const Footer = () => {
   return (
     <Container>
